92d5d30abc9fe10e531fdfa09e0d84c5f7df9cf3
Mukasa Matthew <matthewmukasa0@gmail.com>
2024-11-19 11:00:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+//service import
+const my_business_logic =require('./service/my_business_logic');
+const app = express()
+const port = 3000
+//GET
+//
 app.get('/', (req, res) => {
-res.send('Hello World!');
+  res.send('hiii World!!!!')
 });
+
+
+// List of friends
+let friends = ["Sarah","Oscar","Peter","Nabirah",""];
+app.get ('/list-of-friends',(request ,response) =>{
+  return response.send(friends);
+});
+
+//LIST OF FRIENDS
+let friends_ids = [{"id":1,"name":"Sarah"},
+  {"id":2,"name":"Oscar"},
+  {"id":3,"name":"Peter"},
+  {"id":2,"name":"Nabirah"}];
+
+  app.get('/list-of-friends-by-id',(request ,response) => {
+return response.send(friends_ids)
+  });
+
+//POST
+app.post('/sign-up',(request, response) =>{
+  console.log("Logging request body",request.body);
+
+});
+
+
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ my_db.js
@@ -0,0 +1,8 @@
+let friends_ids = [
+    {"id":1,"name":"Sarah"},
+    {"id":2,"name":"Oscar"},
+    {"id":3,"name":"Peter"},
+    {"id":2,"name":"Nabirah"}];
+
+module.exports = friends_ids;
+
